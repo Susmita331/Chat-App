774811907dcfe3e74f7d2d3657b1d289cf1e0c1c
Susmita331 <susmitachakrabarty215@gmail.com>
2025-09-27 11:07:31 -0600

    Modify App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Left from './home/left/left'
+import Right from './home/right/right'
 function App(){
   return (
     <>
     App
+    <Left></Left>
+    <Right></Right>
     </>
   )
 }
