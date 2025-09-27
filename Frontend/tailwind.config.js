4df6420d3e2728240b339c2e3b1384351478f835
Susmita331 <susmitachakrabarty215@gmail.com>
2025-09-27 10:37:34 -0600

    Add tailwind

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",   // covers React/JS/TS files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
