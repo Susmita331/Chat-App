1fd91bf5b692b5635f158735e7da7ba5c62679ab
Susmita331 <susmitachakrabarty215@gmail.com>
2025-09-27 19:52:26 -0600

    Configure PORT in index.js with .env

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,13 @@
-const express = require('express')
+import express from "express"
+import dotenv from "dotenv"
 const app = express()
-const port = 5002
+dotenv.config();
+const PORT = process.env.PORT || 5001;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
 })
