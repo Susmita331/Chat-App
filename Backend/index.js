bda2d1ce34fe645021c9510b0c8230415992e535
Susmita331 <susmitachakrabarty215@gmail.com>
2025-09-27 21:40:37 -0600

    Install bcrypt.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,10 @@
 import express from "express"
 import dotenv from "dotenv"
 import mongoose from "mongoose"
+import userRoute from "./route/user.route.js"
 const app = express()
 dotenv.config();
+app.use(express.json())
 const PORT = process.env.PORT || 5001;
 const URI = process.env.MONGODB_URI;
 
@@ -13,6 +15,7 @@ try {
     console.log(error)
 
 }
+app.use("/user", userRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
