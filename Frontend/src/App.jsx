2a47a741b76f1a6a51ba9ff15605f5b366228871
Susmita331 <susmitachakrabarty215@gmail.com>
2025-09-28 03:27:44 -0600

    Create logout page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 import Left from './home/left/left'
 import Right from './home/right/right'
 import Logout from './home/left1/Logout'
+import Signup from './components/Signup'
 function App(){
   return (
     <>
-    <div className='flex h-screen'>
-    <Logout></Logout>
-    <Left></Left>
-    <Right></Right>
-    </div>
+    /<div className='flex h-screen'>
+    <Left />
+    <Right />
+    <Signup></Signup>
+    <Login></Login>
+    <Logout />
+</div> 
+  
     
     </>
   )
