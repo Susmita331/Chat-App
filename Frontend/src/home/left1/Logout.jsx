34f9b36a95ed09923d041bb39351bdf0c4fdf15d
Susmita331 <susmitachakrabarty215@gmail.com>
2025-09-27 18:14:57 -0600

    Add ChatUser component

--- Frontend/src/home/left1/Logout.jsx
+++ Frontend/src/home/left1/Logout.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { MdLogout } from "react-icons/md";
 export default function Logout(){
     return (
-        <>
-        <div className='w-[70%] bg-slate-950 text-white'>
-            Logout
+        
+        <div className='w-[5%] bg-slate-950 text-white flex flex-col justify-end'>
+            <div className="p-3">
+                  <form action="">
+                    <div className="flex space-x-3">
+                      <button>
+                        <MdLogout className='text-5xl p-2 hover:bg-gray-600 rounded-lg duration-300'></MdLogout>
+                      </button>
+                    </div>
+                  </form>
+                </div>
         </div>
-        </>
+        
     )
 }
--- Frontend/src/home/right/right.jsx
+++ Frontend/src/home/right/right.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import ChatUser from './ChatUser'
 export default function Right(){
     return (
         <>
         <div className='w-[70%] bg-slate-950 text-white'>
-            Right
+            
+            <ChatUser></ChatUser>
         </div>
         </>
     )
