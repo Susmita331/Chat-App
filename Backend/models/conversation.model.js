eae2c90d74ce549f0f132819c30f7cc4ea2a5b91
Susmita331 <susmitachakrabarty215@gmail.com>
2025-09-29 10:45:31 -0600

    Add Conversation model with members and messages referencing User and Message

--- Backend/models/conversation.model.js
+++ Backend/models/conversation.model.js
@@ -1,25 +1,25 @@
 import mongoose from "mongoose";
+import User from "../models/user.model.js";
+import Message from "message.model.js";
 
-const messageSchema = new mongoose.Schema(
+const conversationSchema = new mongoose.Schema(
   {
-    senderId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    receiverId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    message: {
-      type: String,
-      required: true,
-    },
+    members: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: User,
+      },
+    ],
+    messages: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: Message,
+        default: [],
+      },
+    ],
   },
   { timestamps: true }
 );
 
-const Message = mongoose.model("message", messageSchema);
-
-export default Message;
+const Conversation = mongoose.model("conversation", conversationSchema);
+export default Conversation;
